ecf5351b7b4c832fa6fa1e6452a55f521d2776ae
Jhonny Mendoza <jhomen11@gmail.com>
2022-05-31 11:52:09 -0400

    editar heroe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap} from 'rxjs/operators'
+
 import { Heroe, Publisher } from '../../interfaces/heroe.interface';
 import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [`
+    img{
+      width: 100%;
+      border-radius: 3px;
+    }
+  `]
 })
 export class AgregarComponent implements OnInit {
 
@@ -28,21 +35,42 @@ export class AgregarComponent implements OnInit {
     characters: '',
     publisher: Publisher.DCComics,
     alt_img: ''
-
   }
 
-  constructor( private heroesServices: HeroesService) { }
+  constructor( private heroesServices: HeroesService,
+                private activatedRoute: ActivatedRoute,
+                private route: Router) { }
 
   ngOnInit(): void {
+
+    if(!this.route.url.includes('editar')){
+      return
+    }
+    this.activatedRoute.params
+    .pipe(
+      switchMap(({id})=> this.heroesServices.getHeroePorId(id))
+    )
+    .subscribe(heroe => this.heroe = heroe)
   }
 
   guardar(){
-    if(this.heroe.superhero.trim().length === 0){
+    if(this.heroe.superhero.trim().length === 0 || this.heroe.alt_img?.trim().length === 0){
       return
     }
-    this.heroesServices.agregarHeroe( this.heroe).subscribe( resp => {
-      console.log('Respuesta', resp)
-    } )
+    
+    if( this.heroe.id){
+      //Actualizar
+      this.heroesServices.actualizarHeroe(this.heroe)
+        .subscribe( heroe => {
+          this.route.navigate(['/heroes/', heroe.id])
+        })
+    }else{
+      //Crear nuevo registro
+      this.heroesServices.agregarHeroe( this.heroe)
+      .subscribe( resp => {
+        this.route.navigate(['/heroes/listado'])
+      } )
+    }
   }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -28,4 +28,12 @@ export class HeroesService {
   agregarHeroe( heroe: Heroe): Observable<Heroe>{
     return this.http.post<Heroe>(`${this.baseUrl}/heroes`, heroe)
   }
+
+  actualizarHeroe( heroe: Heroe): Observable<Heroe>{
+    return this.http.put<Heroe>(`${this.baseUrl}/heroes/${heroe.id}`, heroe)
+  }
+
+  eliminarHeroe( heroe: Heroe): Observable<Heroe>{
+    return this.http.delete<Heroe>(`${this.baseUrl}/heroes/${heroe.id}`)
+  }
 }
